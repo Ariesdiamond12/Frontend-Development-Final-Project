c36caebd112bfa56df772cc2419796c5af78b02f
Ariesdiamond12 <92827167+Ariesdiamond12@users.noreply.github.com>
2024-04-29 17:08:51 +0200

    Final Project
    Clipboard Landing Page

---
+++ index.js
@@ -0,0 +1,65 @@
+/******************************Hero Section*******************************/
+
+document.getElementById("");
+let header  = document.getElementById("header").innerHTML = "A history of everything you copy";
+
+document.getElementById("");
+let slogan = document.getElementById("slogan").innerHTML = "Clipboard allows you to track and organise everything you copy. Instantly <br> access your clipboard on all your devives.";
+
+document.getElementById("");
+let title = document.getElementById("title").innerHTML = "Keep track of your snippets";
+
+
+/******************************Content Section****************************/
+
+document.getElementById("");
+let titleText = document.getElementById("titleText").innerHTML = "Clipboard Instantly stores any item you copy in the cloud, meaning you can access your snippets immediately on all your devices.Our Mac and IOS apps will help you organize everything.";
+
+let contentHeading1 = document.getElementById("contentHeading1").innerHTML = "Quick Search";
+console.log(contentHeading1);
+
+let contentSubtext1 = document.getElementById("contentSubtext1").innerHTML = "Easily search your snippets by content,<br>category, web address, application, and more.";
+console.log(contentSubtext1);
+
+let contentHeading2 = document.getElementById("contentHeading2").innerHTML = "iCloud Sync";
+console.log(contentHeading2);
+
+let contentSubtext2 = document.getElementById("contentSubtext2").innerHTML = "Instantly saves and syncs snippets across all your devices.";
+console.log(contentSubtext2);
+
+let contentHeading3 = document.getElementById("contentHeading3").innerHTML = "Complete History";
+console.log(contentHeading3);
+
+let contentSubtext3 = document.getElementById("contentSubtext3").innerHTML = "Retrieve any snippets from the first moment you started using the app.";
+console.log(contentSubtext3);
+
+
+/***************************** */
+let subHeading1 = document.getElementById("subHeading1").innerHTML = "Access Clipboard anywhere";
+console.log(subHeading1);
+let subText1 = document.getElementById("subText1").innerHTML = "Whether you're on the go, or at your computer, you can access all your Clipboard <br> snippets in a few simple clicks.";
+console.log(subText1);
+let subHeading2 = document.getElementById("subHeading2").innerHTML = "Supercharge your workflow";
+console.log(subHeading2);
+let subText2 = document.getElementById("subText2").innerHTML = "We've got the tools to boost your productivity.";
+console.log(subText2);
+
+/*****************************Feature Section******************************/
+let featureHeading1 = document.getElementById("featureHeading1").innerHTML = "Create icon-blacklist";
+console.log(featureHeading1);
+let featureText1 = document.getElementById("featureText1").innerHTML = "Ensure sensitive information never makes its way to your clipboard by excluding certain sources.";
+console.log(featureText1);
+let featureHeading2 = document.getElementById("featureHeading2").innerHTML = "Plain text snippets";
+console.log(featureHeading2);
+let featureText2 = document.getElementById("featureText2").innerHTML = "Remove unwanted formatting from copied text for a consistent look.";
+console.log(featureText2);
+let featureHeading3 = document.getElementById("featureHeading3").innerHTML = "Sneak preview";
+console.log(featureHeading3);
+let featureText3 = document.getElementById("featureText3").innerHTML = "Quick preview of all snippets on your Clipboard for easy access.";
+console.log(featureText3);
+
+
+let accessible = document.getElementById("accessible").innerHTML = "Clipboard for IOS and Mac OS";
+console.log(accessible);
+let accessibleText = document.getElementById("accessibleText").innerHTML = "Available for free on the App Store. Download for Mac or IOS, sync with iCloud <br> and you're ready to start adding to yhoue clipboard.";
+console.log(accessibleText);
